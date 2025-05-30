c0689a013ee185059a6282edf6d41b457b9a0054
medvedevm <medvedevm@telekom.com>
2025-05-30 23:30:35 +0300

    refactored testAccordionAnswersText: moved expected answers from class' variables to another class (class of constants). Also refactored test in such a way it runs set of parametrized tests

--- src/test/java/com/yandex/scooter/test/FaqTest.java
+++ src/test/java/com/yandex/scooter/test/FaqTest.java
@@ -1,30 +1,47 @@
 package com.yandex.scooter.test;
 
+import com.yandex.scooter.constants.FaqAnswers;
 import com.yandex.scooter.pom.MainPage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class FaqTest {
 
+    private final int questionIndex;
+    private final String expectedAnswer;
+
+    public FaqTest(int questionIndex, String expectedAnswer) {
+        this.questionIndex = questionIndex;
+        this.expectedAnswer = expectedAnswer;
+    }
+
     private WebDriver driver;
-    private static final String[] EXPECTED_ANSWERS = {
-        "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-        "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
-        "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
-        "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
-        "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
-        "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
-        "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
-        "Да, обязательно. Всем самокатов! И Москве, и Московской области."
-    };
+    @Parameterized.Parameters(name = "FAQ вопрос #{0}")
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][]{
+            {0, FaqAnswers.PAYMENT_INFO},
+            {1, FaqAnswers.ONE_SCOOTER},
+            {2, FaqAnswers.RENT_START},
+            {3, FaqAnswers.FROM_TOMORROW},
+            {4, FaqAnswers.NO_SUPPORT_CHAT},
+            {5, FaqAnswers.FULL_CHARGE},
+            {6, FaqAnswers.CANCEL_FREE},
+            {7, FaqAnswers.DELIVERY_AREA}
+        });
+    }
 
     @Before
     public void setUp() {
@@ -34,21 +51,16 @@ public class FaqTest {
     }
 
     @Test
-    public void testAccordionAnswersText() {
-
+    public void testAccordionAnswerText() {
         MainPage mainPage = new MainPage(driver);
-
         List<WebElement> questions = mainPage.getAllQuestions();
+        WebElement question = questions.get(questionIndex);
 
-        for (int i = 0; i < questions.size(); i++) {
-            WebElement question = questions.get(i);
-            mainPage.clickQuestion(question);
+        mainPage.clickQuestion(question);
+        WebElement answer = mainPage.getAnswerElementForQuestion(question);
+        String actualText = answer.getText().trim();
 
-            WebElement answer = mainPage.getAnswerElementForQuestion(question);
-            String actualText = answer.getText().trim();
-
-            assertEquals("Текст ответа для вопроса #" + (i + 1) + " не совпадает с ожидаемым", EXPECTED_ANSWERS[i], actualText);
-        }
+        assertEquals("Текст ответа не совпадает для вопроса #" + questionIndex, expectedAnswer, actualText);
     }
 
     @After
