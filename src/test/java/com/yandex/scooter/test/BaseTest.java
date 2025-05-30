050533ca835521c10359880d88557a8a539f33f0
medvedevm <medvedevm@telekom.com>
2025-05-30 23:55:36 +0300

    Улучшила: "Url "https://qa-scooter.praktikum-services.ru/" обычно выделяют как константу (можно реализовать в классе MainPage)."

--- src/test/java/com/yandex/scooter/test/BaseTest.java
+++ src/test/java/com/yandex/scooter/test/BaseTest.java
@@ -7,13 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 
+    public static final String CHROMEDRIVER_PATH = "/Users/A13039316/Downloads/chromedriver-mac-arm64/chromedriver";
+    public static final String URL = "https://qa-scooter.praktikum-services.ru/";
     protected WebDriver driver;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/Users/A13039316/Downloads/chromedriver-mac-arm64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
         driver = new ChromeDriver();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.get(URL);
     }
 
     @After
